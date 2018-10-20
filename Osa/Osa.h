d58bf3c5be31f28a2a9429ece0c4e6e13d667025
Wolchy <Wolchy@users.noreply.github.com>
2018-10-20 03:11:49 -0400

    Started work on Osa
    Time for bed :3

--- Osa/Osa.h
+++ Osa/Osa.h
@@ -2,9 +2,15 @@
 
 #include <iostream>
 
+#include <SDL.h>
+#include <glew.h>
+#include <SDL_opengl.h>
+#include <gl\glu.h>
+#include <SDL_image.h>
+
 class Osa {
 public:
-	std::string OSA_VERSION = "Osa v2.0.0";
+	std::string OSA_VERSION = "Osa v1.0.0";
 	int SCREEN_WIDTH = 1280, SCREEN_HEIGHT = 720;
 
 	Osa(std::string title, int width, int height);
@@ -13,5 +19,7 @@ public:
 	void render();
 
 private:
-
+	bool run = false;
+	SDL_Window* window;
+	SDL_GLContext context;
 };
